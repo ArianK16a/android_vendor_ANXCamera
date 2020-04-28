994cb7a0642a1d25f962f192c127b4969b554628
Arian <arian.kulmer@web.de>
2020-04-28 12:32:59 +0200

    ANXCamera: fix init

--- init/anxfilecheck.sh
+++ init/anxfilecheck.sh
@@ -7,14 +7,14 @@ if [ ! -d ${ANX_DIR} ]; then
     mkdir -p ${ANX_DIR}
 fi
 
-if [ -f /system/etc/ANXCamera/cheatcodes/feature_${DEVICE} || -f /system/etc/device_features/${DEVICE}.xml ]; then
+if [[ -f /system/etc/ANXCamera/cheatcodes/feature_${DEVICE} && -f /system/etc/device_features/${DEVICE}.xml ]]; then
     new_cheatcode=$(sha1sum /system/etc/ANXCamera/cheatcodes/feature_${DEVICE} | cut -d " " -f1)
     old_cheatcode=$(sha1sum ${ANX_DIR}/cheatcodes_reference/feature_${DEVICE} | cut -d " " -f1)
 
     new_features=$(sha1sum /system/etc/device_features/${DEVICE}.xml | cut -d " " -f1)
     old_features=$(sha1sum ${ANX_DIR}/features_reference/${DEVICE}.xml | cut -d " " -f1)
 
-    if [ "$new_cheatcode" = "$old_cheatcode" || "$new_features" = "$old_features" ]; then
+    if [[ "$new_cheatcode" = "$old_cheatcode" || "$new_features" = "$old_features" ]]; then
         echo "anxfilecheck: You are using the latest cheatcode and feature files."
     else
         mkdir ${ANX_DIR}/cheatcodes/
